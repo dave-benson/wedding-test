0c4ac68c4358fdba1d6a90b74b8e45865ab26d07
dave-benson <39385427+dave-benson@users.noreply.github.com>
2018-06-30 15:46:23 +0100

    Update app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -56,25 +56,3 @@ if (t < 0) {
     }
     }); */
 
-
-
-
-
-
-*********card flip****************
-
-
-
-$(document).on('click','.flip',function(){
-	let card =  $(this).closest('.card')
-	if(card.hasClass('flip-it')) card.removeClass('flip-it')
-	else card.addClass('flip-it')
-})
-
-
-$('.card').each(function(){
-	let href = $(this).data('href')
-	$(this).find('.image').css({
-		backgroundImage:['url(',href,')'].join('')
-	})
-})
